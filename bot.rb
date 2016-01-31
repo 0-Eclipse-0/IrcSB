6fc9ed77e43f0d73fe717dae4b80fbf458750f76
0-Eclipse-0 <matthew.hambrecht@gmail.com>
2016-02-01 07:28:09 +0800

    Create bot.rb

---
+++ bot.rb
@@ -0,0 +1,31 @@
+require 'cinch'
+
+f = File.open('logs.txt', 'w')
+
+speaker = nil
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = ARGV[0]
+    c.channels = [ARGV[1]]
+    c.nick = 'EclipseBot'
+  end
+
+  on :message, /(.+)/ do |m, query|
+  	nick = m.user.nick
+
+  	if m.action?
+  		f.write("#{nick} ")
+  	elsif speaker != nick
+		f.write("#{nick} says ")
+  	end
+
+  	text = m.action? ? m.action_message : query
+
+  	speaker = nick
+  	f.write("#{text}\n")
+  	f.flush
+  end
+end
+
+bot.start
